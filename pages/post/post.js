83a949b7c88d6f67780c1977631705f63f92dc78
Danjie Hu <danjiehu@outlook.com>
2021-04-08 00:37:47 +0800

    integrated user log-in, able to upload up to 3 images, integrated horizontal scroll

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,21 +1,47 @@
 // pages/post/post.js
+const app = getApp()
+
+// start of page
 Page({
 
   data: {
-    
+    userInfo: wx.getStorageSync('userInfo'),
+    images: []
   },
 
   onLoad: function () {
-
+   
   },
 
   submitPost: function(e) {
     console.log("submitPost",e)
   },
 
+  // start of uploadImage function
   uploadImage: function(e){
-    console.log("uploadImage",e)
+    wx.chooseImage({
+      count: 3,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        console.log("chooseImage",res)
+        const tempFilePaths = res.tempFilePaths
+        this.setData({
+          images: tempFilePaths
+        })
+      }
+    })
+  },
+  // end of uploadImage function
+
+  // start of previewImage function
+  preview(){
+    wx.previewImage({
+      urls: this.data.images,
+    })
   }
+  // end of previewImage function
 
 
+// end of page
 })
